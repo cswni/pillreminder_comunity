ae2631d2579f4d8dd24363bccea27e12e67ae4e6
daljo25 <camalion007@gmail.com>
2024-11-02 15:40:23 +0100

    feat: Add logo png and svg - Add View logo and logo dark

---
+++ resources/views/filament/admin/logo-dark.blade.php
@@ -0,0 +1,4 @@
+<div class="inline-flex center items-center">
+    <img src="{{ asset('images/logo/logo-dark.svg') }}" alt="PillReminder Logo" class="h-8 w-8">
+    <article class="text-3xl font-semibold text-white">PillReminder</article>  
+</div>
